852b55d85dc855be5e31d0e0d6a738f9a0f24633
tjsgh1217 <tjsgh1217@gmail.com>
2025-03-21 01:17:21 +0900

    docs: 콘솔 로그 추가

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config();
   console.log(process.env.AWS_REGION);
-
+  console.log(process.env.AWS_ACCESS_KEY_ID);
   const app = await NestFactory.create(AppModule);
 
   await app.listen(process.env.PORT ?? 3000);
